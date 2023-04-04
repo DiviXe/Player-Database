90a0c809b6d6bda4a165c46d66a152570de1f753
Leo Ahopalo <bgs208@myy-haaga.helia.fi>
2023-04-04 12:41:59 +0300

    JPA tests fully functioning

--- src/test/java/com/game/PlayerDatabase/DatabaseRepositoryTests.java
+++ src/test/java/com/game/PlayerDatabase/DatabaseRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.game.PlayerDatabase;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.game.PlayerDatabase.domain.Player;
@@ -18,6 +20,8 @@ import com.game.PlayerDatabase.domain.Server;
 import com.game.PlayerDatabase.domain.ServerComputer;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
 public class DatabaseRepositoryTests {
 	
 	@Autowired
@@ -30,84 +34,83 @@ public class DatabaseRepositoryTests {
 	ServerComputerRepository sprepository;
 	
 // PLAYER TESTS
-	// MARIA DB ISSUES
-//	@Test
-//	public void findByPlayerName() {
-//		List<Player> players = prepository.findByplayerName("Aramed");
-//		assertThat(players.get(0).getPlayerName().equalsIgnoreCase("Aramed"));	
-//		
-//	}
+	@Test
+	public void findByPlayerName() {
+		List<Player> players = prepository.findByplayerName("Decker");
+		assertThat(players.get(0).getPlayerName().equalsIgnoreCase("Decker"));	
+		
+	}
 	
-	// MARIA DB ISSUES
-//	@Test
-//	public void CreateNewPlayer() {
-//		Player player = new Player("test", "test Testi", 1997, "testi@gmail.com", "testitappi55", srepository.findByServerName("Randuins").get(0));
-//		prepository.save(player);
-//		assertThat(player.getId()).isNotNull();
-//	}
+	@Test
+	public void CreateNewPlayer() {
+		Player player = new Player("testiii", "Testi Teppo", 1997, "testi123@gmail.com", "testitappi55", srepository.findByServerName("Randuins").get(0));
+		prepository.save(player);
+		assertThat(player.getId()).isNotNull();
+	}
 	
 
 	@Test
 	public void findPlayerOnServer() {
-		List<Player> players = prepository.findByServerServerName("Randuins");
-		assertThat(players).hasSize(0);
+		List<Player> players = prepository.findByServerServerName("Albama");
+		assertThat(players).hasSize(1);
 	}
 	
-// EI toimi koska validoinnissa on paljon mitkä blockkaavat saven. 
-//	@Test
-//	public void SavePlayer() {
-//		Player player = new Player();
-//		prepository.save(player);
-//		assertNotEquals((Long) player.getId(), null);
-//	}
-	
-// MARIA DB ISSUES 
-//	@Test
-//	public void updatePlayer() {
-//	    Optional<Player> player = prepository.findById((long) 1);
-//	    assertNotNull(player.get().getId());
-//	    player.get().setEmail("asd@gmail.com");
-//	    List<Player> players = prepository.findByEmail("asd@gmail.com");
-//	    assertThat(players).size().isEqualTo(0);
-//	}
+
+	@Test
+	public void updatePlayer() {
+	    Optional<Player> player = prepository.findById((long) 1);
+	    assertNotNull(player.get().getId());
+	    player.get().setEmail("asd@gmail.com");
+	    List<Player> players = prepository.findByEmail("asd@gmail.com");
+	    assertThat(players).size().isEqualTo(1);
+	}
 	
 //SERVER TESTS
-// MARIA DB ISSUES
-//	@Test
-//	public void findServerName() {
-//		List<Server> servers = srepository.findByServerName("Randuins");
-//		assertThat(servers.get(0).getServerName().equalsIgnoreCase("Randuins"));
-//
-//	}
-	
-	@Test 
-	public void createNewServer() {
-		Server server = new Server("testi", 2000);
-		srepository.save(server);
-		assertThat(server.getId()).isNotNull();
+	@Test
+	public void findServerName() {
+		List<Server> servers = srepository.findByServerName("Randuins");
+		assertThat(servers.get(0).getServerName().equalsIgnoreCase("Randuins"));
+
 	}
 	
+	@Test
+	public void updateServer() {
+		Optional<Server> server = srepository.findById((long) 1);
+		assertNotEquals(server.get().getId(), null);
+		server.get().setServerName("testi123");
+		List<Server> servers = srepository.findByServerName("testi123");
+		assertThat(servers).hasSize(1);
+	}
 	
 //SERVER COMPUTER TESTS
-	// MARIA DB ISSUES
-//	@Test
-//	public void FindByComputerName() {
-//	List<ServerComputer> servercomputers = sprepository.findByComputerName("Windows_1");
-//	assertThat(servercomputers.get(0).getComputerName().equalsIgnoreCase("Windows_1"));
-//	}
+	@Test
+	public void FindByComputerName() {
+	List<ServerComputer> servercomputers = sprepository.findByComputerName("Windows_2");
+	assertThat(servercomputers.get(0).getComputerName().equalsIgnoreCase("Windows_2"));
+	}
 	
-	// MARIA DB ISSUES
-//	@Test 
-//	public void createNewServerComputer() {
-//		ServerComputer sp = new  ServerComputer("Windows_Testi", "OK", "180.0.0.0", srepository.findByServerName("Randuins").get(0));
-//		sprepository.save(sp);
-//		assertThat(sp.getId()).isNotNull();
-//	}
+	@Test 
+	public void createNewServerComputer() {
+		ServerComputer sp = new  ServerComputer("Windows_Test2", "OK_OK", "180.152.52.62", srepository.findByServerName("Albama").get(0));
+		sprepository.save(sp);
+		assertThat(sp.getId()).isNotNull();
+	}
 	
 	@Test 
 	public void FindServerNameOnServerComputer() {
-		List<ServerComputer> servercomputers = sprepository.findByServerServerName("Randuins");
-		assertThat(servercomputers).hasSize(0);
+		List<ServerComputer> servercomputers = sprepository.findByServerServerName("Albama");
+		
+		assertThat(servercomputers).size().isEqualTo(1);
+	}
+	
+	@Test
+	public void UpdateServerComputer() {
+		Optional<ServerComputer> servercomputer = sprepository.findById((long) 1);
+		assertNotEquals(servercomputer.get().getId(), null);
+		servercomputer.get().setComputerName("test_computer1");
+		List<ServerComputer> servercomputers = sprepository.findByComputerName("test_computer1");
+		assertThat(servercomputers).hasSize(1);
 		
 	}
+	
 }
