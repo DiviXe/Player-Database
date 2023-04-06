dd7a56fd13054cdbbf897a0dbf2d19426cbf36dd
Leo Ahopalo <bgs208@myy-haaga.helia.fi>
2023-04-06 15:23:31 +0300

    rest fixes

--- src/main/java/com/game/PlayerDatabase/web/RestDatabaseController.java
+++ src/main/java/com/game/PlayerDatabase/web/RestDatabaseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,34 +33,40 @@ public class RestDatabaseController {
 
 		
 		//RESTful way to show all players
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("players")
 		public @ResponseBody List<Player> playerListRest() {
 			return (List<Player>) prepository.findAll();
 		}
 		
 		//RESful way to see all servers
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("servers")
 		public @ResponseBody List<Server> serverListRest() {
 			return (List<Server>) srepository.findAll();
 		}
 		//RESful way to see all server computers
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("servercomputers")
 		public @ResponseBody List<ServerComputer> serverComputerListRest() {
 			return (List<ServerComputer>) screpository.findAll();
 		}
 		
 		//Player by ID REST
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("/player/{id}")
 		public @ResponseBody Optional<Player> findPlayerRest(@PathVariable("id") Long playerId) {
 			return prepository.findById(playerId);
 		}
 		//Server by ID REST
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("/server/{id}")
 		public @ResponseBody Optional<Server> findServerRest(@PathVariable("id") Long serverId) {
 		return srepository.findById(serverId);
 				}
 				
 	    //Servercomputer by ID REST
+		@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 		@GetMapping("/servercomputer/{id}")
 		public @ResponseBody Optional<ServerComputer> findServerComputerRest(@PathVariable("id") Long serverComputerId) {
 		return screpository.findById(serverComputerId);
